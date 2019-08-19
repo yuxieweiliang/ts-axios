5166696a9bcc71196fbedfb8a5d1d6d8330ea7ee
雨歇微凉 <1413074908@126.com>
2019-08-19 11:19:35 +0800

    feat(all the file): init project
    init project by typescript library starter

---
+++ test/02-ts-axios.test.ts
@@ -0,0 +1,14 @@
+import DummyClass from '../src/02-ts-axios'
+
+/**
+ * Dummy test
+ */
+describe('Dummy test', () => {
+  it('works if true is truthy', () => {
+    expect(true).toBeTruthy()
+  })
+
+  it('DummyClass is instantiable', () => {
+    expect(new DummyClass()).toBeInstanceOf(DummyClass)
+  })
+})
